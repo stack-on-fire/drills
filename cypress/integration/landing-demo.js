f713b4b22517bddd7bed9f15d8e6f136c955a2de
d-ivashchuk <dimaiv@me.com>
2022-02-20 09:26:28 +0100

    fix tests

--- cypress/integration/landing-demo.js
+++ cypress/integration/landing-demo.js
@@ -2,16 +2,19 @@ describe("JS drills", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
-  // it("shows demo drill", () => {
-  //   cy.get("h2").should("have.text", "⚡️Drills⚡️");
-  // });
-  // it("has correct & functioning buttons buttons", () => {
-  //   cy.get("[data-cy=prettier]").click();
-  //   cy.get("[data-cy=hints]").click();
-  //   cy.get("[data-cy=hints]").click();
-  //   cy.get("[data-cy=hints]").click();
-  //   cy.get("[data-cy=tester]").should("be.disabled");
-  // });
+  it("shows demo drill", () => {
+    cy.get("h2").should(
+      "have.text",
+      "⚡️Drills⚡️Become JavaScript professional"
+    );
+  });
+  it("has correct & functioning buttons buttons", () => {
+    cy.get("[data-cy=prettier]").click();
+    cy.get("[data-cy=hints]").click();
+    cy.get("[data-cy=hints]").click();
+    cy.get("[data-cy=hints]").click();
+    cy.get("[data-cy=tester]").should("be.disabled");
+  });
   it("can solve the problem", () => {
     cy.get(".cm-content").click().clear();
     cy.get(".cm-activeLine")
@@ -28,8 +31,7 @@ describe("JS drills", () => {
       .click()
       .type(
         `;export default doubleNumber;
-    `,
-        { force: true, delay: 10 }
+    `
       );
 
     cy.get("[data-cy=prettier]").click();
