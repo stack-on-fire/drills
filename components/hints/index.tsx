d45bdd0e0be9fcd0a3e3183202ab0b92834e7767
d-ivashchuk <dimaiv@me.com>
2022-02-20 08:47:13 +0100

    revise hints

--- components/hints/index.tsx
+++ components/hints/index.tsx
@@ -1,8 +1,9 @@
-import { Box, theme, VStack } from "@chakra-ui/react";
+import { Box, HStack, theme, VStack } from "@chakra-ui/react";
 import React, { Dispatch, SetStateAction } from "react";
 import ReactMarkdown from "react-markdown";
 import { motion, AnimatePresence } from "framer-motion";
 import { DrillWithHintsAndTestCases } from "types/drill";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
 
 const MotionBox = motion(Box);
 
@@ -14,22 +15,28 @@ type Props = {
 
 const Hints = ({ visibleHints, drill }: Props) => {
   return (
-    <VStack align="left">
+    <VStack zIndex={100} position="fixed" bottom="10" align="left">
       <AnimatePresence>
         {drill.hints.slice(0, visibleHints).map((hint) => {
           return (
             <MotionBox
+              key={hint.id}
+              layout
+              layoutId={hint.id}
               borderRadius={4}
               px={4}
               py={2}
               background="gray.700"
               _last={{ marginBottom: theme.sizes[2] }}
-              key={hint.id}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
+              animate={{ opacity: 1, y: 0 }}
+              initial={{ opacity: 0, y: 20 }}
               exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
             >
-              <ReactMarkdown>{hint.text}</ReactMarkdown>
+              <HStack>
+                <InfoOutlineIcon color="yellow.400" />
+                <ReactMarkdown>{hint.text}</ReactMarkdown>
+              </HStack>
             </MotionBox>
           );
         })}
